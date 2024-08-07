7e38a6d95aad358bf4870a85c4a24d7ca083c127
Valentina <valentina@g.ncu.edu.tw>
2024-08-07 21:55:53 +0800

    初始化project

---
+++ springboot-mall/src/main/java/com/valentina/springbootmall/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.valentina.springbootmall.service;
+
+import com.valentina.springbootmall.model.Product;
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+}
